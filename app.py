b8041f6f613b085ebbdb58b9c6a1c5dd07ce10ec
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-15 10:57:47 -0500

    add variable to store the outputs de pygame.init() and if conditional to validate if has found any error during the initialization

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ difficulty_level = 25
 frame_size_h = 720
 frame_size_v = 480
 
+
+# looking for errors
+check_errors = pygame.init()
+#print(check_errors) output: (5,0) <---tuple
+
+if check_errors[1] > 0:
+    print(f"[!] Had {check_errors[1]} errors when initializing game, exiting...")
+    sys.exit(-1)
+else:
+    print("[+] Game successfully initialized")
