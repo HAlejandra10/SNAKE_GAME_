7603fac40ad868b2412722c12451f7a60ff2ca0f
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-15 14:58:46 -0500

    add position to the score part w/ if conditional

--- app.py
+++ app.py
@@ -65,6 +65,11 @@ def show_score(choice, color, font, size):
     score_font = pygame.font.SysFont(font, size)
     score_surface = score_font.render("Score : " + str(score), True, color)
     score_rect = score_surface.get_rect()
+    
+    if choice == 1:
+        score_rect.midtop = (frame_size_h/10, 15)
+    else:
+        score_rect.midtop = (frame_size_h/2, frame_size_v/1.25)
 
 
 
