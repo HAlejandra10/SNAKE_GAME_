a7c3443b72ed468621eb9c5e264a97dbda3d9c29
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-15 12:58:58 -0500

    add game_over function to declare the message when you lost the game playing w/ styles and to define frame into the rectangule about the game, update of screen about the changes

--- app.py
+++ app.py
@@ -45,5 +45,21 @@ change_to = direction
 #game score
 score= 0 
 
+# When Game Over
+def game_over():
+    my_font = pygame.font.SysFont('times new', 90)
+    game_over_surface= my_font.render("YOU DIED!", True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop= (frame_size_h/2, frame_size_v/4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    show_score(0, red, 'times', 20)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
+
+
+
 
 
