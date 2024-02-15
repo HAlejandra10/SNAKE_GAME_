1cdea493ffcacc1cebb52367828e470e3e7fdb11
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-15 17:06:26 -0500

    draw rectangule into the game window with parameter(Rect) define position and dimentions of rectangule

--- app.py
+++ app.py
@@ -127,6 +127,16 @@ while True:
         food_position =[random.randrange(1, (frame_size_h//10)) *10, random.randrange(1, (frame_size_v//10)) *10]
     food_generate = True
 
+    #GFX
+    game_window.fill(black)
+    
+    for pos in snake_body_position:
+        # Snake body
+        # .draw.rect(play_surface, color, xy-coordinate)
+        # xy-coordinate -> .Rect(x, y, size_x, size_y)
+        #10, 10 = width, height
+        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+        
     
 
 
