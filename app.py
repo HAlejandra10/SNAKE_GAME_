cdba88bdeded1b243be1914b5ab8508d7a1db4a1
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-15 11:16:16 -0500

    adding colors on the game screen

--- app.py
+++ app.py
@@ -20,3 +20,12 @@ else:
 # to initialize game window
 pygame.display.set_caption("Snake Snacker")
 game_window = pygame.display.set_mode((frame_size_h, frame_size_v))
+
+# putting colors on screen
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+
